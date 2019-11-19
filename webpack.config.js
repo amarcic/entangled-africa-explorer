f55c42d45876a70a02762dbf6fe725dd26d51b2b
elli <ereuhl1@uni-koeln.de>
2019-11-19 14:38:54 +0100

    webpack, babel, react setup

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+var HtmlWebpackPlugin =  require('html-webpack-plugin');
+
+module.exports = {
+    entry : './src/index.js',
+    output : {
+        path : path.resolve(__dirname , 'dist'),
+        publicPath: '/',
+        filename: 'index_bundle.js'
+    },
+    resolve: {
+        modules: [__dirname + './src', 'node_modules'],
+        extensions: ['.js', '.jsx']
+    },
+    devServer: {
+        contentBase: './dist',
+        historyApiFallback: true
+    },
+    module : {
+        rules : [
+            {test : /\.(js|jsx)$/, exclude: /node_modules/, use:'babel-loader'},
+            {test : /\.css$/, use:['style-loader', 'css-loader']}
+        ]
+    },
+//    mode:'development',
+    plugins : [
+        new HtmlWebpackPlugin ({
+            template : 'src/index.html'
+        })
+    ]
+
+}
+
+
