f7866525187a253b6bda2a9054ffdec55bd33ef6
amarcic <aleksander.marcic@gmail.com>
2019-11-26 14:28:04 +0100

    basic folder structure for components

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,15 @@ module.exports = {
     },
     module : {
         rules : [
-            {test : /\.(js|jsx)$/, exclude: /node_modules/, use:'babel-loader'},
-            {test : /\.css$/, use:['style-loader', 'css-loader']}
+            {
+                test : /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use:'babel-loader'
+            },
+            {
+                test : /\.css$/,
+                use:['style-loader', 'css-loader']
+            }
         ]
     },
 //    mode:'development',
