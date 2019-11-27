b82467d38b600c2b175a12d21841d9cd7c19488e
amarcic <aleksander.marcic@gmail.com>
2019-11-27 13:30:43 +0100

    added explaining comments to index.js in components folder

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,4 @@
+//this index.js exports all components from the sub folders
+//by doing this all components can be imported from './components/';
+//named exports are used instead of default export; thus imports need to be destructured 
 export { HelloComputerButton } from './HelloComputerButton/HelloComputerButton';
