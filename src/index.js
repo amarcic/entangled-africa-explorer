90e5534659dae1d57788a3f8401e2aab6b264121
amarcic <aleksander.marcic@gmail.com>
2019-12-04 20:47:27 +0100

    changed class component to function component; testing hooks

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { HelloComputerButton } from './components/';
 import './index.css';
 
-class App extends React.Component{
-    render(){
-        return(
-            <div>
-                <span>Hello World</span><br />
-                <HelloComputerButton />
-            </div>
-        )
+const INIT_LABELS = {
+    HelloWorld: {
+        de: "Hallo ganze Welt",
+        en: "Hello World"
+    },
+    HelloComputerButton: {
+        de: "Achtung, Rechenmaschine!",
+        en: "Hello Computer"
     }
+};
+
+const INIT_SETTINGS = {
+    language: "en"
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const App = () => {
+    const [labels, setLabels] = useState(INIT_LABELS);
+    const [settings, setSettings] = useState(INIT_SETTINGS);
+
+    return(
+        <div>
+            <span>{labels.HelloWorld[settings.language]}</span><br />
+            <HelloComputerButton />
+        </div>
+        )
+    }
+
+ReactDOM.render(<App />, document.getElementById('app' ))
