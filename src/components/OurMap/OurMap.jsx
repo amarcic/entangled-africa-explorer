9179aebbf6783ffedd4cd9e8009c508c138ec70c
amarcic <aleksander.marcic@gmail.com>
2020-03-24 14:50:41 +0100

    added GraphQL useQuery hook to map

--- src/components/OurMap/OurMap.jsx
+++ src/components/OurMap/OurMap.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Icon } from 'leaflet';
+
+import { useQuery } from "@apollo/react-hooks";
+import gql from 'graphql-tag';
 
 const osmTiles = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const osmAttr = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 const mapCenter = [11.5024338, 17.7578122];
 const zoomLevel = 4;
+
+const GET_OBJECT_WITH_CONTEXT = gql`
+    query giveInfo {
+        entity(id: 1189042) {
+            name
+            temporalArachne {
+                begin
+            }
+            related {
+                identifier
+                name
+                spatial {
+                    name
+                    coordinates
+                }
+            }
+        }
+    }
+`;
 
 export const OurMap = () => {
     const {t, i18n} = useTranslation();
@@ -14,9 +37,12 @@ export const OurMap = () => {
         i18n.changeLanguage(lng);
     };
 
+    const { data, loading, error } = useQuery(GET_OBJECT_WITH_CONTEXT);
+
     return(
         <div>
             <h2>{t('Map')}</h2>
+            {data? data.entity.name :  <p>no data found</p>}
             <Map
                 center={mapCenter}
                 zoom={zoomLevel}
