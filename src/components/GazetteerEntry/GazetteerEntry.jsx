a8d57feb19c3a098cd7437aa89a2f2129afada5b
elli <ereuhl1@uni-koeln.de>
2020-09-17 17:21:49 +0200

    Added expand functionality and more info to results table

---
+++ src/components/GazetteerEntry/GazetteerEntry.jsx
@@ -0,0 +1,53 @@
+import { Table, TableBody, TableCell, TableRow } from "@material-ui/core";
+import React from "react";
+
+export const GazetteerEntry = (props) => {
+    //itemCoordinates and itemLocation are used if this information is coming from a different field than for most items
+    const { item, itemCoordinates, itemLocation } = props;
+
+    return (
+        <React.Fragment>
+            <h4>{item.name}</h4>
+            <Table size="small">
+                <TableBody>
+                    <TableRow>
+                        <TableCell component="th" scope="row">Namen:</TableCell>
+                        <TableCell>{item.name}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell component="th" scope="row">Identifier:</TableCell>
+                        <TableCell>{item.identifier}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell component="th" scope="row">Lage:</TableCell>
+                        <TableCell>{item.coordinates || itemCoordinates}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell component="th" scope="row">Art:</TableCell>
+                        <TableCell>?</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell component="th" scope="row">Kontexte:</TableCell>
+                        <TableCell>?</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell component="th" scope="row">Liegt in:</TableCell>
+                        <TableCell>{(item.locatedIn && item.locatedIn.name) || itemLocation}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell component="th" scope="row">Tags:</TableCell>
+                        <TableCell>?</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell component="th" scope="row">Provenienz:</TableCell>
+                        <TableCell>?</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell component="th" scope="row">Datensatzgruppe:</TableCell>
+                        <TableCell>?</TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </React.Fragment>
+    )
+}
--- src/components/ResultsTableRow/ResultsTableRow.jsx
+++ src/components/ResultsTableRow/ResultsTableRow.jsx
@@ -1,41 +1,70 @@
-import {TableCell, TableRow, Tooltip} from "@material-ui/core";
+import {Box, Collapse, Table, TableBody, TableCell, TableHead, TableRow, Tooltip} from "@material-ui/core";
 import RoomIcon from "@material-ui/icons/Room";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
-import React from "react";
+import React, { useState } from 'react';
+import { ArachneEntry, GazetteerEntry } from "..";
+
 
 export const ResultsTableRow = (props) => {
     //itemCoordinates and itemLocation are used if this information is coming from a different field than for most items
     const { index, item, itemCoordinates, itemLocation, mode, openPopup } = props;
 
+    //state used to open/close (expand/collapse) TableRows
+    const [open, setOpen] = useState(false);
+
     return (
-        <TableRow>
-            <TableCell>
-                {(item.coordinates || itemCoordinates)
-                    ? (<Tooltip title="Show on map" arrow placement="right">
-                        <RoomIcon
-                            fontSize="small"
-                            onClick={() => openPopup(index)}
-                        />
-                    </Tooltip>)
-                    : "(No coordinates)"}
-            </TableCell>
-            <TableCell>
-                {item.name}
-            </TableCell>
-            <TableCell>
-                {(item.locatedIn && item.locatedIn.name) || itemLocation}
-            </TableCell>
-            <TableCell>
-                <Tooltip title="View original entry in iDAI.world" arrow placement="right">
-                    <a href={
-                        mode==="archaeoSites"
-                            ? `https://gazetteer.dainst.org/place/${item.identifier}`
-                            : `https://arachne.dainst.org/entity/${item.identifier}`
-                    } target="_blank" rel="noreferrer">
-                        <ExitToAppIcon/>
-                    </a>
-                </Tooltip>
-            </TableCell>
-        </TableRow>
+        <React.Fragment>
+            <TableRow
+                onClick={() => setOpen(!open)}
+            >
+                <TableCell>
+                    {(item.coordinates || itemCoordinates)
+                        ? (<Tooltip title="Show on map" arrow placement="right">
+                            <RoomIcon
+                                fontSize="small"
+                                onClick={() => openPopup(index)}
+                            />
+                        </Tooltip>)
+                        : "(No coordinates)"}
+                </TableCell>
+                <TableCell>
+                    {item.name}
+                </TableCell>
+                <TableCell>
+                    {(item.locatedIn && item.locatedIn.name) || itemLocation}
+                </TableCell>
+                <TableCell>
+                    <Tooltip title="View original entry in iDAI.world" arrow placement="right">
+                        <a href={
+                            mode==="archaeoSites"
+                                ? `https://gazetteer.dainst.org/place/${item.identifier}`
+                                : `https://arachne.dainst.org/entity/${item.identifier}`
+                        } target="_blank" rel="noreferrer">
+                            <ExitToAppIcon/>
+                        </a>
+                    </Tooltip>
+                </TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={4}>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <Box margin={1}>
+                            {mode==="archaeoSites"
+                                ? <GazetteerEntry
+                                    item={item}
+                                    itemCoordinates={itemCoordinates}
+                                    itemLocation={itemLocation}
+                                />
+                                : <ArachneEntry
+                                    item={item}
+                                    itemCoordinates={itemCoordinates}
+                                    itemLocation={itemLocation}
+                                />
+                            }
+                        </Box>
+                    </Collapse>
+                </TableCell>
+            </TableRow>
+        </React.Fragment>
     );
 };
