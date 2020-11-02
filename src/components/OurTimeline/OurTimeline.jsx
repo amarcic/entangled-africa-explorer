ae744754e2ec4b3eb2aa8b7f5a82a358c16a974d
elli <ereuhl1@uni-koeln.de>
2020-11-02 19:43:00 +0100

    Added svg rect for each timeSpan to timeline

--- src/components/OurTimeline/OurTimeline.jsx
+++ src/components/OurTimeline/OurTimeline.jsx
@@ -1,8 +1,7 @@
-import React, {useEffect, useRef, useState} from "react";
-import { Grid } from "@material-ui/core";
-import { useTranslation } from "react-i18next";
-import * as d3 from "d3";
-import { useQuery } from "@apollo/react-hooks";
+import React, {useEffect, useState} from "react";
+import {Grid} from "@material-ui/core";
+import {useTranslation} from "react-i18next";
+import {useQuery} from "@apollo/react-hooks";
 import gql from 'graphql-tag';
 
 
@@ -42,51 +41,70 @@ export const OurTimeline = () => {
             <Grid className="grid-outer" container direction="row" spacing={1}>
                 <Grid className="grid-timeline" item xs={12} lg={9}>
                     <svg
-                        //viewBox="0 0 100 50"
-                        style={{ border: "2px solid gold" }}>
-
+                        viewBox="-1000 0 2000 1000"
+                        style={{ border: "2px solid gold" }}
+                    >
+                        {/*coordinate 0,0 (= year 0)*/}
                         <circle
-                            cx="100"
-                            cy="100"
+                            cx="0"
+                            cy="0"
                             r="10"
-                            fill="yellow"
-                        />
-                        <rect
-                            width="10"
-                            height="20"
-                            x="200"
-                            y="50"
-                            fill="blue"
-                        />
-                        <rect
-                            width="30"
-                            height="20"
-                            x="50"
-                            fill="green"
-                        />
-                        <rect
-                            width="30"
-                            height="20"
-                            x="150"
-                            y="25"
-                            fill="orange"
                         />
 
+                        {/*draw a rect for each datingSpan:
+                         starting point x of the rect = the first element/year of the datingSpan
+                         width of the rect = difference between first and second year of the datingSpan
+                         offset the rects along the y-axis using the index value; TODO: find better solution for overlapping
+                         if the width of the rect would be 0 because datingSpan[0]===datingSpan[1], draw a circle instead
+                         */}
                         {timelineData && timelineData.entitiesByString && timelineData.entitiesByString.map((item, index) => {
-                                item && item.datingSpan && item.datingSpan.map((dating, index) => {
-                                        return (
-                                            dating &&
-                                            <circle
-                                                cx="10"
-                                                cy="50"
-                                                r="3"
-                                                fill="red"
-                                            />
-                                            && console.log("draw a red circle!")
+                            return (item.datingSpan && item.datingSpan.map((ds, dsIndex) => {
+                                        const calculatedWidth = Math.abs(Math.abs(ds[0]) - Math.abs(ds[1])); //TODO: calculate in a better was, because e.g. [-100, 100] would not work right now
+                                        return (ds
+                                            && (calculatedWidth === 0
+                                                    ? <g key={"datingSpan_" + index + "_" + dsIndex}>
+                                                        <circle
+                                                            cx={ds[0]}
+                                                            cy={index + 8}
+                                                            r="4"
+                                                            strokeWidth="1"
+                                                            stroke="black"
+                                                            fill="gold"
+                                                        />
+                                                        <text
+                                                            x={ds[0]}
+                                                            y={index * 5 + 10}
+                                                            fill="black"
+                                                        >
+                                                            {item.name}
+                                                        </text>
+                                                    </g>
+                                                    : <g key={"datingSpan_" + index + "_" + dsIndex}>
+                                                        <rect
+                                                            x={ds[0]}
+                                                            y={index * 5}
+                                                            width={calculatedWidth}
+                                                            height="10"
+                                                            strokeWidth="1"
+                                                            stroke="black"
+                                                            rx="2"
+                                                            ry="2"
+                                                            fill="gold"
+                                                        />
+                                                        <text
+                                                            x={ds[0]}
+                                                            y={index * 5 + 10}
+                                                            fill="black"
+                                                        >
+                                                            {item.name}
+                                                        </text>
+                                                    </g>
+                                            )
                                         );
-                                })
-                            }
-                        )}
+                                    }
+                                )
+                            );
+                        })}})}})}
                     </svg>
                 </Grid>
             </Grid>
