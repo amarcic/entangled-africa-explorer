ee281b352619affc9cd3ac51e99a145832b856ba
elli <ereuhl1@uni-koeln.de>
2020-11-04 19:36:17 +0100

    Added functions for filtering and sorting the timeline data

--- src/components/OurTimeline/OurTimeline.jsx
+++ src/components/OurTimeline/OurTimeline.jsx
@@ -33,7 +33,28 @@ export const OurTimeline = () => {
             setTimelineData(dataTimelineData);
             console.log("rerender timelineData --> timelineData: ", timelineData);
         }
-    }, [dataTimelineData]);
+    }, [dataTimelineData, input.searchStr]);
+
+
+    //filter functions for timeline data:
+    //filter out elements that do not have a datingSpan specified
+    const filterNoDatingSpan = (element) => {
+        if (element.datingSpan !== null && element.datingSpan.length !== 0) return element;
+    }
+    //filter out elements that have more than one datingSpan (just temporarily)
+    const filterMultipleDatingSpans = (element) => {
+        if (element.datingSpan.length === 1) return element;
+    }
+
+    //sort functions for timeline data:
+    //sort by identifier in descending order (just a test)
+    const sortByIdentifierDescending = (a, b) => {
+        return b.identifier - a.identifier;
+    }
+    //sort by year; TODO: is based on datingSpan[0][0], if there is more than one datingSpan given this is not taken into account so far
+    const sortYearAscending = (itemA, itemB) => {
+        return itemA.datingSpan[0][0] - itemB.datingSpan[0][0];
+    }
 
     return (
         <div>
@@ -44,67 +65,71 @@ export const OurTimeline = () => {
                         viewBox="-1000 0 2000 1000"
                         style={{ border: "2px solid gold" }}
                     >
-                        {/*coordinate 0,0 (= year 0)*/}
-                        <circle
-                            cx="0"
-                            cy="0"
-                            r="10"
-                        />
+                        {//mark coordinate 0,0 (= year 0) with a circle
+                            <circle
+                                cx="0"
+                                cy="0"
+                                r="10"
+                            />}
 
-                        {/*draw a rect for each datingSpan:
-                         starting point x of the rect = the first element/year of the datingSpan
-                         width of the rect = difference between first and second year of the datingSpan
-                         offset the rects along the y-axis using the index value; TODO: find better solution for overlapping
-                         if the width of the rect would be 0 because datingSpan[0]===datingSpan[1], draw a circle instead
-                         */}
-                        {timelineData && timelineData.entitiesByString && timelineData.entitiesByString.map((item, index) => {
-                            return (item.datingSpan && item.datingSpan.map((ds, dsIndex) => {
-                                        const calculatedWidth = Math.abs(Math.abs(ds[0]) - Math.abs(ds[1])); //TODO: calculate in a better was, because e.g. [-100, 100] would not work right now
-                                        return (ds
-                                            && (calculatedWidth === 0
-                                                    ? <g key={"datingSpan_" + index + "_" + dsIndex}>
-                                                        <circle
-                                                            cx={ds[0]}
-                                                            cy={index + 8}
-                                                            r="4"
-                                                            strokeWidth="1"
-                                                            stroke="black"
-                                                            fill="gold"
-                                                        />
-                                                        <text
-                                                            x={ds[0]}
-                                                            y={index * 5 + 10}
-                                                            fill="black"
-                                                        >
-                                                            {item.name}
-                                                        </text>
-                                                    </g>
-                                                    : <g key={"datingSpan_" + index + "_" + dsIndex}>
-                                                        <rect
-                                                            x={ds[0]}
-                                                            y={index * 5}
-                                                            width={calculatedWidth}
-                                                            height="10"
-                                                            strokeWidth="1"
-                                                            stroke="black"
-                                                            rx="2"
-                                                            ry="2"
-                                                            fill="gold"
-                                                        />
-                                                        <text
-                                                            x={ds[0]}
-                                                            y={index * 5 + 10}
-                                                            fill="black"
-                                                        >
-                                                            {item.name}
-                                                        </text>
-                                                    </g>
-                                            )
-                                        );
-                                    }
-                                )
-                            );
-                        })}})}})}
+                        {timelineData && timelineData.entitiesByString && timelineData.entitiesByString
+                            .filter(filterNoDatingSpan) //filter out elements where no datingSpan is specified
+                            .filter(filterMultipleDatingSpans) //temporary fix for imperfect sortYearAscending function
+                            .sort(sortYearAscending) //sort elements by year in ascending order
+                            .map((item, index) => {
+                                return (item.datingSpan && item.datingSpan.map((ds, dsIndex) => {
+                                            const calculatedWidth = Math.abs(Math.abs(ds[0]) - Math.abs(ds[1])); //TODO: calculate in a better was, because e.g. [-100, 100] would not work right now
+                                            return (ds &&
+                                                //
+                                                (calculatedWidth !== 0
+                                                        //draw a rect for each datingSpan:
+                                                        //starting point x of the rect = the first element/year of the datingSpan,
+                                                        //width of the rect = difference between first and second year of the datingSpan,
+                                                        //offset the rects along the y-axis using the index value; TODO: find better solution for overlapping
+                                                        ? <g key={"datingSpan_" + index + "_" + dsIndex}>
+                                                            <rect
+                                                                x={ds[0]}
+                                                                y={index * 10}
+                                                                width={calculatedWidth}
+                                                                height="10"
+                                                                strokeWidth="1"
+                                                                stroke="black"
+                                                                rx="2"
+                                                                ry="2"
+                                                                fill="gold"
+                                                            />
+                                                            <text
+                                                                x={ds[0]}
+                                                                y={index * 10 + 10}
+                                                                fill="black"
+                                                            >
+                                                                {item.name}
+                                                            </text>
+                                                        </g>
+                                                        //if the width of the rect would be 0 because datingSpan[0] === datingSpan[1], draw a circle instead
+                                                        : <g key={"datingSpan_" + index + "_" + dsIndex}>
+                                                            <circle
+                                                                cx={ds[0]}
+                                                                cy={index * 10}
+                                                                r="5"
+                                                                strokeWidth="1"
+                                                                stroke="black"
+                                                                fill="gold"
+                                                            />
+                                                            <text
+                                                                x={ds[0]}
+                                                                y={index * 10 + 10}
+                                                                fill="black"
+                                                            >
+                                                                {item.name}
+                                                            </text>
+                                                        </g>
+                                                )
+                                            );
+                                        }
+                                    )
+                                );
+                            })}})}})}
                     </svg>
                 </Grid>
             </Grid>
